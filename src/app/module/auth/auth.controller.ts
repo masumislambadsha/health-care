e0776138cd8fa0dbed7d197aab736594e36f13a4
masumislambadsha <nkfhhdndjdh@gmail.com>
2026-08-18 21:59:07 +0600

    fix: remove unnecessary imports

--- src/app/module/auth/auth.controller.ts
+++ src/app/module/auth/auth.controller.ts
@@ -4,9 +4,6 @@ import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
 import type { IRequestUser } from "./auth.interface";
 import { AuthService } from "./auth.service";
-import z, { email } from "zod";
-import { PatientRegistrationZodSchema } from "./auth.validation";
-
 
 const registerPatient = catchAsync(async (req: Request, res: Response) => {
 	const payload = req.body
