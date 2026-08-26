fbcf56592228eaf9a844ec35dbf98e023eae351a
masumislambadsha <nkfhhdndjdh@gmail.com>
2026-08-26 14:49:26 +0600

    feat: destroy old profile image after new upload

--- src/app/module/user/user.service.ts
+++ src/app/module/user/user.service.ts
@@ -1,43 +1,58 @@
 import { cloudinary } from "../../lib/cloudinary";
 import { prisma } from "../../lib/prisma";
 
-const uploadProfileImage = async (buffer: Buffer, userId: string) =>
-
-  {
-   const cloudinaryResult =cloudinary.uploader.upload_stream(
-    {
-      resource_type: "auto",
+const uploadProfileImage = async (buffer: Buffer, userId: string) => {
+  const currentUser = await prisma.user.findUnique({
+    where: {
+      id: userId,
     },
-    async (error, result) =>  {
-      if(error){
-        console.log("error" , error.message)
-        throw new Error(error.message)
-      }
+    select: {
+      imagePublicId: true,
+      imageUrl: true,
+    },
+  });
 
-      // console.log("result ",result)
-
-      const updatedUser = await prisma.user.update({
-        where: {
-          id: userId
-        },
-        data: {
-          imageUrl: result?.secure_url,
-          imagePublicId: result?.public_id
+  const cloudinaryResult = cloudinary.uploader
+    .upload_stream(
+      {
+        resource_type: "auto",
+      },
+      async (error, result) => {
+        if (error) {
+          console.log("error", error.message);
+          throw new Error(error.message);
         }
-      })
 
-      console.log(updatedUser)
-    },
-  ).end(buffer);
+        // console.log("result ",result)
+
+        const updatedUser = await prisma.user.update({
+          where: {
+            id: userId,
+          },
+          data: {
+            imageUrl: result?.secure_url,
+            imagePublicId: result?.public_id,
+          },
+        });
+
+        console.log(updatedUser);
+      },
+    )
+    .end(buffer);
   const user = await prisma.user.findUnique({
     where: {
-      id: userId
+      id: userId,
     },
-    omit:{
-      password: true
-    }
-  })
+    omit: {
+      password: true,
+    },
+  });
 
+  if(currentUser?.imagePublicId && currentUser?.imageUrl ) {
+    await cloudinary.uploader.destroy(currentUser.imagePublicId)
+  }
+
+  return user
 };
 
 export const UserService = {
